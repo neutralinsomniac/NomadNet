a7ec7ae9ec1d1ea81280a2eccfc32359c3ae161e
Mark Qvist <bc7291552be7a58f361522990465165c>
2026-07-10 16:28:39 +0200

    Updated versions

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setuptools.setup(
     entry_points= {
         'console_scripts': ['nomadnet=nomadnet.nomadnet:main']
     },
-    install_requires=["rns>=1.3.5", "lxmf>=1.0.1", "urwid>=3.0.5", "qrcode"],
+    install_requires=["rns>=1.3.8", "lxmf>=1.0.1", "urwid>=3.0.5", "qrcode"],
     python_requires=">=3.8",
 )
